473c069704ac11a85832f37d1723568d3852ccfc
TomaruDev <tomaru.development@gmail.com>
2020-12-24 22:53:13 +0100

    Basic load module

--- src/args.js
+++ src/args.js
@@ -1,5 +1,13 @@
+const fs = require("fs");
+
 module.exports = {
 	load: (app) => {
 		this.app = app;
+		this.args = new Map();
+
+		fs.readdirSync(__dirname + "/args").forEach((file) => {
+			let arg = require(__dirname + "/args/" + file);
+			this.args.set(arg.man.name, arg);
+		});
 	}
 };
