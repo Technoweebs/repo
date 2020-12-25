1f316df67ef84f09db172e3abbfe375c512ff77e
TomaruDev <tomaru.development@gmail.com>
2020-12-25 17:47:43 +0100

    Module Manager

--- src/modules.js
+++ src/modules.js
@@ -1,5 +1,21 @@
+const fs = require("fs");
+
 module.exports = {
 	load: (app) => {
 		this.app = app;
+		this.modules = new Map();
+
+		fs.readdirSync(__dirname + "/modules").forEach((file) => {
+			let module = require(__dirname + "/modules/" + file);
+			this.modules.set(module.name, arg);
+		});
+	},
+
+	check: (module) => {
+		return this.modules.get(module) != undefined;
+	},
+
+	exec: (module, options) => {
+		return this.check(module) ? this.modules.get(module).exec(options) : null;
 	}
 };
